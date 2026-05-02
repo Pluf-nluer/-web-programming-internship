6cf70818f5c3148fb13f0f1d4c376f310b5d3535
Pluf_nluer <23130240@st.hcmuaf.edu.vn>
2026-05-02 23:53:01 +0700

    feat: them DAO đe luu featered vao bang

--- src/main/java/com/example/backend/dao/FeaturedProductDAO.java
+++ src/main/java/com/example/backend/dao/FeaturedProductDAO.java
@@ -1,4 +1,46 @@
 package com.example.backend.dao;
 
+import com.example.backend.util.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class FeaturedProductDAO {
+
+    public boolean isFeaturedGenerated(int month, int year) {
+        String sql = "SELECT COUNT(*) FROM featured_products WHERE month=? AND year=?";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, month);
+            ps.setInt(2, year);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) return rs.getInt(1) > 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void saveFeatured(int productId, int categoryId, int month, int year) {
+        String sql = """
+        INSERT IGNORE INTO featured_products(product_id, category_id, month, year)
+        VALUES (?, ?, ?, ?)
+    """;
+
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setInt(1, productId);
+            ps.setInt(2, categoryId);
+            ps.setInt(3, month);
+            ps.setInt(4, year);
+            ps.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
